07f119fb49df181ee66d68beaa14fe7e7faf950e
Darius2020-2021 <mohamedtine077@gmail.com>
2021-02-26 14:21:40 +0000

    voici la calculatrice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-preuve';
+  num1:number = 0
+  num2:number = 0
+  reponGlobal:number=0
+  resultatAdd(){
+    this.reponGlobal=this.num1+this.num2
+
+  }
+  resultatSous(){
+    this.reponGlobal=this.num1-this.num2
+
+  }  
+  resultatMulti(){
+    this.reponGlobal=this.num1*this.num2
+
+  }  
+  resultatDiv(){
+    this.reponGlobal=this.num1/this.num2
+
+  }
+
+
 }
