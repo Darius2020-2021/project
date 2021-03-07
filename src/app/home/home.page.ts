ff9bc7aea5fa38d7ba766a1f5ae05d7cfab1c084
Darius2020-2021 <mohamedtine077@gmail.com>
2021-03-07 13:21:50 +0000

    voilà

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  num1: number = 0
+  num2: number = 0
+  reponGlobal: number = 0
+  operaAdd(){
+    this.reponGlobal = this.num1+this.num2
+    return console.log(this.reponGlobal)
+  }
+  operaSous(){
+    this.reponGlobal = this.num1-this.num2
+    return console.log(this.reponGlobal)
+  }
+  operaMult(){
+    this.reponGlobal = this.num1*this.num2
+    return console.log(this.reponGlobal)
+  }
+  operaDiv(){
+    this.reponGlobal = this.num1/this.num2
+    return console.log(this.reponGlobal)
+  }
   constructor() {}
 
 }
